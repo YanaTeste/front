e46ef9618227b9da1d2e3503db0f300cc5eb16c2
Roberto Ornelas <rob@front.no>
2012-05-02 22:57:14 +0100

    Refactor frontSearch behavior, lay out responsive framework, temporary unset unecessary scripts, make menu responsive

--- sites/all/themes/frontd7/scripts/script.js
+++ sites/all/themes/frontd7/scripts/script.js
@@ -9,21 +9,10 @@
             if ($('#header .search-form', context).length) {
                 var searchForm = $('#header .search-form', context);
 
-                $('input.form-submit', searchForm).click(function(e) {
-                    if (searchForm.hasClass('form-open')) {
-                        return true;
-                    }
-                    else {
-                        searchForm.addClass('search-active');
-                        $('#main-menu:visible', context).fadeOut();
-                        e.preventDefault();
-                        $('input.form-text', searchForm).focus();
-                    }
-                });
-
-                $('*', searchForm).blur(function() {
-                    searchForm.removeClass('search-active');
-                    $('#main-menu:hidden', context).fadeIn();
+                $('input.form-submit', searchForm).live('click', function(e) {
+                    e.preventDefault();
+                    searchForm.toggleClass('search-active');
+                    $('#main-menu', context).fadeToggle();
                 });
             }
         }

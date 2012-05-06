d3120dca94f7aa62cd859d0f2943ea87df8281f9
Roberto Ornelas <rob@front.no>
2012-05-06 23:42:11 +0100

    Nodequeue module + home services theming

--- sites/all/themes/frontd7/templates/page.tpl.php
+++ sites/all/themes/frontd7/templates/page.tpl.php
@@ -130,7 +130,7 @@
       <h1 class="title" id="page-title"><?php print $title; ?></h1>
     <?php endif; ?>
     <?php print render($title_suffix); ?>
-    <?php if ($tabs): ?>
+    <?php if (isset($tabs)): ?>
       <div class="tabs"><?php print render($tabs); ?></div>
     <?php endif; ?>
     <?php print $messages; ?>

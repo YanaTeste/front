9a36abc4dab59c6221012cc7d6552f45f018a2da
Henrik Akselsen <henrik.akselsen@gmail.com>
2013-02-06 14:49:36 +0100

    #460 Only add some namespace to make feed validate

--- sites/all/themes/frontd7/templates/views/views-view-rss.tpl.php
+++ sites/all/themes/frontd7/templates/views/views-view-rss.tpl.php
@@ -8,7 +8,7 @@
  */
 ?>
 <?php print "<?xml"; ?> version="1.0" encoding="utf-8" <?php print "?>"; ?>
-<rss version="2.0" xml:base="<?php print $link; ?>"<?php print $namespaces; ?>>
+<rss version="2.0" xml:base="<?php print $link; ?>"<?php print 	'xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:content="http://purl.org/rss/1.0/modules/content/" xmlns:foaf="http://xmlns.com/foaf/0.1/" xmlns:rdfs="http://www.w3.org/2000/01/rdf-schema#"'; ?>>
   <channel>
     <title><?php print $title; ?></title>
     <link><?php print $link; ?></link>
